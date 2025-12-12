8100233c44be846a9569994b1f09666e73901519
Ziza60 <denisiomp@bol.com.br>
2025-12-11 21:46:32 -0300

    Updated poseDetector.ts

--- utils/poseDetector.ts
+++ utils/poseDetector.ts
@@ -21,7 +21,7 @@ export async function initializePoseDetector(): Promise<void> {
 
     poseLandmarker = await PoseLandmarker.createFromOptions(vision, {
       baseOptions: {
-        modelAssetPath: 'https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/1/pose_landmarker_lite.tflite',
+        modelAssetPath: 'https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/1/pose_landmarker_lite.task',
         delegate: 'GPU'
       },
       runningMode: 'IMAGE',
